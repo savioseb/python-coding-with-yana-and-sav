61883ebbc2ad19b180b50991c333d7e8b39027e0
Savio <savioseb@gmail.com>
2023-09-04 06:22:40 +0530

    Miscellaneous changes - added read me file

--- 202309/program_to_print_yana_and_savio.py
+++ 202309/program_to_print_yana_and_savio.py
@@ -12,7 +12,10 @@ def main():
     for name in list_of_names:
         for i in range(len(name)+1):
             print( f'{name[:i]}')
-            time.sleep( 1 )
+            time.sleep( 1 )     # 1 second sleep
+            # time.sleep( 10 )  # 10 seconds
+            # time.sleep( 0 )   # no sleep
+            # time.sleep( -1 )  # fails
 
 if __name__ == '__main__':
     main()
---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Coding with Yana and Sav
+
+This is a small repo for walking through code that Yana and Sav does together.
+
